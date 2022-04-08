cf9f9532286606c281250d60d9103a1ee974cac3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-04-09 04:03:06 +0500

    work complete

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
 
 const CryptoDetails = () => {
+  const {coinId}=useParams()
   return (
     <div>
-      Cryptodetails
+      Cryptodetails {coinId}
     </div>
   )
 }
